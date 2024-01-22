4fa025c77ed7d5a51b1f281e295b4cbbeb985c58
tangowithfoxtrot <5676771+tangowithfoxtrot@users.noreply.github.com>
2024-01-22 08:17:08 -0800

    fix: `GPLv3 license header not found in the first 20 lines of the module`

--- plugins/lookup/bitwarden_sm.py
+++ plugins/lookup/bitwarden_sm.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 # (c) 2024, Bitwarden <hello@bitwarden.com>
-# Licensed under the GPL-3.0-or-later
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 from __future__ import absolute_import, division, print_function
 
